3f2532791f3d4c058b3acf9821e8c81f9cb3d2b8
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2019-09-05 15:32:33 -0700

    Add: python e2e test cases

---
+++ tests/python/HttpTrigger/__init__.py
@@ -0,0 +1,10 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse(
+        "OK",
+        status_code=200
+    )
