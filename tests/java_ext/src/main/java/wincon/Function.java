89c571cb7ce68292b3ac2ee10f8cb8193eaace98
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2019-09-09 15:35:15 -0700

    Backup e2e

---
+++ tests/java_ext/src/main/java/wincon/Function.java
@@ -0,0 +1,14 @@
+package wincon;
+
+import java.util.*;
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
+
+public class Function {
+    @FunctionName("HttpTrigger")
+    public HttpResponseMessage run(
+            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+        return request.createResponseBuilder(HttpStatus.OK).body("OK").build();
+    }
+}
