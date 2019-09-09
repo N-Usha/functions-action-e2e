f8b01cf09c7103c743192bdb9d9e7425002421a7
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2019-09-09 15:43:43 -0700

    Test dotnet.yml

--- tests/node/HttpTrigger/index.ts
+++
@@ -1,8 +0,0 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    context.res = { body: "OK" };
-};
-
-export default httpTrigger;
