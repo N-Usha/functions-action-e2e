48f484a38b15689fb3ee0bbff544c15c2b20b03e
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2019-09-10 11:19:13 -0700

    Add lodash

---
+++ tests/node/LodashTrigger/index.ts
@@ -0,0 +1,11 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import * as _ from "lodash";
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.res = {
+        status: 200,
+        body: `Lodash Random Number Generator: ${_.random(0.0, 1.0, true)}`;
+    };
+};
+
+export default httpTrigger;
